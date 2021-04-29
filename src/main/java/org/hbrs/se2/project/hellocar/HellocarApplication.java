3355954269aad4d22ce8c1ccb403827851c37f3e
Michael Welle <michael.welle@h-brs.de>
2021-04-29 10:36:40 +0000

    Update HellocarApplication.java

--- src/main/java/org/hbrs/se2/project/hellocar/HellocarApplication.java
+++ src/main/java/org/hbrs/se2/project/hellocar/HellocarApplication.java
@@ -2,6 +2,8 @@ package org.hbrs.se2.project.hellocar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 public class HellocarApplication extends SpringBootServletInitializer {
