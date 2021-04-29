299ce28b58999c5d620a1dd8cdbf9c902b543e8d
Michael Welle <michael.welle@h-brs.de>
2021-04-29 10:54:42 +0000

    Add new file

---
+++ src/main/java/org/hbrs/se2/project/hellocar/ServletInitializer.java
@@ -0,0 +1,14 @@
+package org.hbrs.se2.project.hellocar; 
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(HellocarApplication.class);
+  }
+
+}
+
