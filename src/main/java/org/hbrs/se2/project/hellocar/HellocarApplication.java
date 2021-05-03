5d8ef4eb9efb4481ad46a1f93b31322ef8e6afca
Michael Welle <pkgs@c0t0d0s0.de>
2021-05-03 13:04:16 +0200

    -

--- src/main/java/org/hbrs/se2/project/hellocar/HellocarApplication.java
+++ src/main/java/org/hbrs/se2/project/hellocar/HellocarApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HellocarApplication { //extends SpringBootServletInitializer {
+public class HellocarApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HellocarApplication.class, args);
