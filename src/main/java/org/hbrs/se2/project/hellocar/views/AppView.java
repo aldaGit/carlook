4cd4f1aec4ac1268aee0d36299bb441a5c533873
saschaalda <sascha.alda@h-brs.de>
2021-06-16 10:19:14 +0200

    Neue Begrüßung

--- src/main/java/org/hbrs/se2/project/hellocar/views/AppView.java
+++ src/main/java/org/hbrs/se2/project/hellocar/views/AppView.java
@@ -207,7 +207,7 @@ public class AppView extends AppLayout implements BeforeEnterObserver {
         viewTitle.setText(getCurrentPageTitle());
 
         // Setzen des Vornamens von dem aktuell eingeloggten Benutzer
-        helloUser.setText("Hello my dear old friend: "  + this.getCurrentNameOfUser() );
+        helloUser.setText("Hello my dear old friend!! "  + this.getCurrentNameOfUser() );
     }
 
     private Optional<Tab> getTabForComponent(Component component) {
