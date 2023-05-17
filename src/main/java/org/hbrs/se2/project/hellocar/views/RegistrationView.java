44eab21b462ee2c9a59d1b63258da4fc29fe6a69
saschaalda <sascha.alda@h-brs.de>
2023-05-17 14:32:55 +0200

    Neue View für Registrierung mit dem Team AldaVia.

---
+++ src/main/java/org/hbrs/se2/project/hellocar/views/RegistrationView.java
@@ -0,0 +1,95 @@
+package org.hbrs.se2.project.hellocar.views;
+
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.UI;
+import org.hbrs.se2.project.hellocar.control.ManageCarControl;
+import org.hbrs.se2.project.hellocar.dtos.impl.CarDTOImpl;
+import org.hbrs.se2.project.hellocar.dtos.UserDTO;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.customfield.CustomField;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import org.hbrs.se2.project.hellocar.util.Globals;
+
+@Route(value = "registration" )
+@PageTitle("User Registration")
+@CssImport("./styles/views/entercar/enter-car-view.css")
+public class RegistrationView extends Div {  // 3. Form (Spezialisierung / Vererbung)
+
+    // ToDo: Namen der Variablen ändern, neue hinzufügen; Validierung (...)
+    private TextField brand = new TextField("E-Mail");
+    private TextField model = new TextField("Name");
+    private TextField description = new TextField("Vorname");
+    private TextField price = new TextField("Password");
+
+    private Button register = new Button("Register");
+
+    private Binder<CarDTOImpl> binder = new Binder(CarDTOImpl.class);
+
+    public RegistrationView(ManageCarControl carService) {
+
+        // ToDo: neues DTO (z.B. RegistrationDTO); neuer Control (RegControl); Binder; Excpetion Handling
+        addClassName("enter-car-view");
+
+        add(createTitle());
+        add(createFormLayout());
+        add(createButtonLayout());
+
+        // Default Mapping of Cars attributes and the names of this View based on names
+        // Source: https://vaadin.com/docs/flow/binding-data/tutorial-flow-components-binder-beans.html
+        binder.bindInstanceFields(this); // Nr. 1 API-Methode
+        clearForm();
+
+        register.addClickListener(e -> {
+            // Speicherung der Daten über das zuhörige Control-Object.
+            // Daten des Autos werden aus Formular erfasst und als DTO übergeben.
+            // Zusätzlich wird das aktuelle UserDTO übergeben.
+            // UserDTO userDTO = (UserDTO) UI.getCurrent().getSession().getAttribute(Globals.CURRENT_USER);
+            // carService.createCar(binder.getBean() ,  userDTO );
+
+            Notification.show("Benutzer wurde registriert.");
+            clearForm();
+
+            // Navigation
+            UI.getCurrent().navigate(Globals.Pages.LOGIN_VIEW);
+        });
+    }
+
+    private void clearForm() {
+        binder.setBean(new CarDTOImpl());
+    }
+
+    private Component createTitle() {
+        return new H3("User Registration");
+    }
+
+    private Component createFormLayout() {
+        FormLayout formLayout = new FormLayout();
+        formLayout.add(brand, model, description, price);
+        return formLayout;
+    }
+
+    private Component createButtonLayout() {
+        HorizontalLayout buttonLayout = new HorizontalLayout();
+        buttonLayout.addClassName("button-layout");
+        register.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        buttonLayout.add(register);
+        return buttonLayout;
+    }
+
+
+
+}
